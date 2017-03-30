035c6eb014dbcdccdc57a7f2b89a55049556ba62
Larry Lade <lplade@users.noreply.github.com>
2017-03-29 23:21:41 -0500

    initial runnable version of api

--- app_api/serializers.py
+++ app_api/serializers.py
@@ -2,20 +2,21 @@ from rest_framework import serializers
 from .models import Ebook, Creator
 
 
-class EbookSerializer(serializers.ModelSerializer):
-    """Maps Model to JSON"""
-
-    # http://stackoverflow.com/questions/20633313/django-rest-framework-get-related-model-field-in-serializer
-    creator = CreatorSerializer(source='creator', read_only=True)
-
-    class Meta:
-        model = Ebook
-        fields = ('ebook_id', 'creator', 'title')  # TODO do anything with other fields?
-
-
 class CreatorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Creator
         fields = ('creator_id', 'name', 'birth_year', 'death_year')  # TODO do something with aliases?
+
+
+class EbookSerializer(serializers.ModelSerializer):
+    """Maps Model to JSON"""
+
+    # http://stackoverflow.com/questions/20633313/django-rest-framework-get-related-model-field-in-serializer
+    creator = CreatorSerializer(read_only=True)
+
+    class Meta:
+        model = Ebook
+        fields = ('ebook_id', 'creator', 'title')  # TODO do anything with other fields?
+
 
